b1477ecf2b2206e161dd3f15e585dadbf775a8fc
1835573610 <85839422+1835573610@users.noreply.github.com>
2024-05-17 14:50:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'wxid_8v41bec1bcqz22',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ncXR-7Teu78yhyg6VF3AuMqMK0zAUJ0F9VtiB10WnlM',
+      useTemplateId: '0aD_8z8y0zfkzDr3Wwf_IYaxaAycgQWYQwYIq9Tfcz4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'nT4zPX1k39CMV3VTcI0-TfPewBIA_mMyuTgnxGxpXCU',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'wxid_8v41bec1bcqz22',
     }
   ],
 
