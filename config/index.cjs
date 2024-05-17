1f807867ffd889c244cc8cfe4f3a2d51cdec820c
1835573610 <85839422+1835573610@users.noreply.github.com>
2024-05-17 15:14:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '老婆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '8v41bec1bcqz22',
+      id: 'Theyboy1',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0aD_8z8y0zfkzDr3Wwf_IYaxaAycgQWYQwYIq9Tfcz4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '8v41bec1bcqz22',
+      id: 'Theyboy1',
     }
   ],
 
